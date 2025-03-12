33c9c6c3741283c2a871301e05bf6a46616cdc55
fermanferdaus <fermanf91@gmail.com>
2025-03-12 13:47:00 +0700

    Update code

--- lemari_hidroponik.ino
+++ lemari_hidroponik.ino
@@ -10,8 +10,9 @@
 #define TDS_PIN 33      // Pin analog tempat sensor TDS terhubung
 #define ONE_WIRE_BUS 32 // Pin digital tempat sensor DS18B20 terhubung
 #define DHTPIN 25
-#define RELAY_LAMPU 26
-#define RELAY_KIPAS 27
+#define RELAY_KIPAS 26
+#define RELAY_LAMPU 27
+#define RELAY_PPM 14
 
 unsigned long previousMillisSensor = 0;
 const long intervalSensor = 5000;
@@ -33,8 +34,8 @@ bool statusKipas = false;
 bool statusLampu = false;
 
 // WiFi setup
-const char* ssid = "aiko";
-const char* password = "menujubersama";
+const char* ssid = "UPCA GANK";
+const char* password = "rondoanaklimo";
 String serverUrl = "http://lemarihidroponik.coeagriculture.my.id/post_data.php";
 
 void setup() {
@@ -42,7 +43,6 @@ void setup() {
 
   dht.begin();
   sensors.begin(); // Inisialisasi sensor suhu
-  timeClient.begin();  // Mulai NTP Client
 
   initializeWiFi();
   initializePins();
@@ -70,6 +70,7 @@ void initializePins(){
   // Set relay pins
   pinMode(RELAY_LAMPU, OUTPUT);
   pinMode(RELAY_KIPAS, OUTPUT);
+  pinMode(RELAY_PPM, OUTPUT);
 }
 
 void initializeWiFi() {
@@ -96,18 +97,10 @@ void readSensors(String timeString) {
   int analogValue = analogRead(TDS_PIN); // Membaca nilai analog dari sensor TDS
 
   float suhuRuang = dht.readTemperature();
-  if (isnan(suhuRuang)) {
-    Serial.println("Gagal membaca suhu ruangan!");
-    return;
-  }
 
   // Membaca suhu dari DS18B20
   sensors.requestTemperatures();
   float suhuAir = sensors.getTempCByIndex(0); // Mengambil suhu dalam Celsius
-  if (suhuAir == DEVICE_DISCONNECTED_C) {
-    Serial.println("Gagal membaca suhu air!");
-    return;
-  }
 
   Serial.print("TDS Value: ");
   Serial.println(analogValue);
@@ -134,10 +127,10 @@ void sendDataToServer(int analogValue, float suhuAir, float suhuRuang) {
   HTTPClient http;
 
   String data = "suhu_air=" + String(suhuAir) + 
-  "&suhu_ruangan=" + String(suhuRuang) + 
-  "&analog_tds=" + String(analogValue) + 
-  "&status_lampu=" + (statusLampu ? "1" : "0") + 
-  "&status_kipas=" + (statusKipas ? "1" : "0");
+                "&suhu_ruangan=" + String(suhuRuang) + 
+                "&analog_tds=" + String(analogValue) + 
+                "&status_lampu=" + (statusLampu ? "1" : "0") + 
+                "&status_kipas=" + (statusKipas ? "1" : "0");
 
   http.begin(serverUrl);
   http.addHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -156,51 +149,32 @@ void sendDataToServer(int analogValue, float suhuAir, float suhuRuang) {
 }
 
 String getFormattedTime() {
-  timeClient.update();                    
-  int hours = timeClient.getHours();
-  int minutes = timeClient.getMinutes();
-  
-  char formattedTime[6]; // Buffer untuk menyimpan format waktu "HH:MM"
-  snprintf(formattedTime, sizeof(formattedTime), "%02d:%02d", hours, minutes);
-  
-  return String(formattedTime);  
-}
-
-void setRelaysLampu(int stateLampu) {
-  digitalWrite(RELAY_LAMPU, stateLampu);
+  timeClient.update();                      // Perbarui waktu dari NTP
+  String formattedTime = timeClient.getFormattedTime();
+  return formattedTime.substring(0, 5);     // Ambil format HH:MM
 }
 
 void setRelaysKipas(int stateKipas) {
   digitalWrite(RELAY_KIPAS, stateKipas);
 }
 
-int convertTimeToMinutes(String timeStr) {
-  int h = timeStr.substring(0, 2).toInt();
-  int m = timeStr.substring(3, 5).toInt();
-  return (h * 60) + m;
-}
-
 void controlRelays(String timeString, float suhuAir) {
-  int currentTime = convertTimeToMinutes(timeString);
-  int startLampu = convertTimeToMinutes(jadwal1);
-  int endLampu = convertTimeToMinutes(jadwal2);
-
-  if (currentTime >= startLampu && currentTime <= endLampu) {
-    setRelaysLampu(HIGH);
+  if (timeString >= jadwal1 && timeString <= jadwal2) {
+    digitalWrite(RELAY_LAMPU, LOW);
     statusLampu = true;
     Serial.println("Lampu Aktif");
   } else {
-    setRelaysLampu(LOW);
+    digitalWrite(RELAY_LAMPU, HIGH);
     statusLampu = false;
     Serial.println("Lampu Mati");
   }
 
   if (suhuAir >= 32) {
-    setRelaysKipas(HIGH);
+    digitalWrite(RELAY_KIPAS, LOW);
     statusKipas = true;
     Serial.println("Kipas Aktif");
   } else {
-    setRelaysKipas(LOW);
+    digitalWrite(RELAY_KIPAS, HIGH);
     statusKipas = false;
     Serial.println("Kipas Mati");
   }
